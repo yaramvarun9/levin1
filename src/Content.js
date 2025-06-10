2f88b48d445c037186dcecf484f28f3b863f0873
yaramvarun9 <yaramvarun9@gmail.com>
2025-06-10 22:32:26 +0530

    click events

--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,25 @@ const Content = () => {
   const changeName = () =>{
     return "Varun";
   }
+  const handleClick = () =>{
+    console.log("You Clicked Here");
+  }
+  const handleClick2 = (name) =>{
+    console.log(`${name} Clicked`);
+  }
+  const handleClick3 = (e) =>{
+    console.log(e);
+    console.log(e.target);
+  }
 
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {changeName()}
         </p>
-
+        <button onClick={handleClick}>Click</button>
+        <button onClick={()=> handleClick2('Varun')}>Click2</button>
+        <button onClick={(e)=> handleClick3(e)}>Click2</button>
     </main>
   )
 }
