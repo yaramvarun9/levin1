efca24e9d75e22747a0fe20f8280e424bc6ccd63
yaramvarun9 <yaramvarun9@gmail.com>
2025-06-11 11:16:08 +0530

    useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const Content = () => {
-  const changeName = () =>{
-    return "Varun";
-  }
+  const [name,setName] = useState('Levin');
+  const [count,setCount] = useState(0);
+
+
   const handleClick = () =>{
+    setName("Chaac");
     console.log("You Clicked Here");
   }
   const handleClick2 = (name) =>{
-    console.log(`${name} Clicked`);
+    setCount(count+1);
+    console.log(count); 
   }
   const handleClick3 = (e) =>{
     console.log(e);
@@ -18,9 +21,9 @@ const Content = () => {
   return (
     <main>
         <p onDoubleClick={handleClick}>
-            Hello {changeName()}
+            Hello {name}
         </p>
-        <button onClick={handleClick}>Click</button>
+        <button onClick={handleClick}>Change Name</button>
         <button onClick={()=> handleClick2('Varun')}>Click2</button>
         <button onClick={(e)=> handleClick3(e)}>Click2</button>
     </main>
